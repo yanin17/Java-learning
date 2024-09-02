b780129133978ea11822cb349ba17ea1eba26ee7
yanin17 <zabakunoyanin@gmail.com>
2024-09-02 14:01:49 +0700

    test constructor

---
+++ Child.java
@@ -0,0 +1,9 @@
+public class Child extends Parent {
+    private int y;
+
+    public static void main(String[] args) {
+        Child child = new Child();
+        System.out.println("Child : Default constructor set y = " + child.y);
+    }
+
+}
---
+++ Parent.java
@@ -0,0 +1,9 @@
+// Test default constructor
+public class Parent {
+    private int x = 3;
+
+    Parent() {
+        System.out.println("Parent: Pareent() constructor set x = " + x);
+
+    }
+}

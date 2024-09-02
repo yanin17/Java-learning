a52f6bf5f99d39e52a69f42a58221a04e04d82b2
yanin17 <zabakunoyanin@gmail.com>
2024-09-02 11:50:00 +0700

    add geometric calcu

---
+++ People.java
@@ -0,0 +1,12 @@
+public class People {
+    private String name;
+    public int age;
+
+    public void setName(String n) {
+        name = n;
+    }
+
+    public String getName() {
+        return name;
+    }
+}

568f50847451392398ae261735c812f26622b798
yanin17 <zabakunoyanin@gmail.com>
2024-09-02 14:31:39 +0700

    test polymorphism

---
+++ TestPolymorphism.java
@@ -0,0 +1,42 @@
+public class TestPolymorphism {
+    String name, dept;
+    int empId;
+    double salary;
+
+    public TestPolymorphism(String name, int empId, double salary) {
+        this.name = name;
+        this.empId = empId;
+        this.salary = salary;
+        this.dept = "N/A";
+    }
+
+    public TestPolymorphism(String name, int empId, double salary, String dept) {
+        this.name = name;
+        this.empId = empId;
+        this.salary = salary;
+        this.dept = dept;
+    }
+
+    public void printData() {
+        System.out.println("Name : " + name);
+        System.out.println("Employee ID : " + empId);
+        System.out.println("Salary : " + salary);
+        System.out.println("Department : " + dept);
+    }
+
+    public static void main(String[] args) {
+        TestPolymorphism tp;
+        if (args.length < 3 || args.length > 4) {
+            System.out.println("Please input data : ");
+            System.out.println("For an employee => Employee ID and Salary");
+            System.out.println("For an maneger => Employee ID and Department");
+        } else if (args.length == 3) {
+            tp = new TestPolymorphism(args[0], Integer.parseInt(args[1]), Double.parseDouble(args[2]));
+            tp.printData();
+        } else if (args.length == 4) {
+            tp = new TestPolymorphism(args[0], Integer.parseInt(args[1]), Double.parseDouble(args[2]), args[3]);
+            tp.printData();
+        }
+    }
+
+}

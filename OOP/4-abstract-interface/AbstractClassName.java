cf610bc0908dfdadd1dbcb64b1b9879c73aa22f9
yanin17 <zabakunoyanin@gmail.com>
2024-09-03 08:26:40 +0700

    add abstract test

---
+++ OOP/4-abstract-interface/AbstractClassName.java
@@ -0,0 +1,7 @@
+package OOP.4-abstract-interface;
+
+// Creating Abstract Class
+abstract class AbstractClassName {
+    // [Abstract MethodName]
+    // [Concrete MethodName]
+}
---
+++ OOP/4-abstract-interface/employee1.java
@@ -0,0 +1,15 @@
+package OOP.4-abstract-interface;
+
+abstract class employee1 {
+    protected String emp_id;
+
+    public abstract void setOT(double amount);
+
+    public double calOT(int mhr, double pr) {
+        return mhr * pr;
+    }
+
+    public void setID(String ID) {
+        emp_id = ID;
+    }
+}

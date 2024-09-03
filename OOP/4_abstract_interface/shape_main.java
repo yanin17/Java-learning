82065478a740214e0abca65fc2fa1c0159945914
yanin17 <zabakunoyanin@gmail.com>
2024-09-03 09:05:03 +0700

    update object

--- OOP/4_abstract_interface/shape_main.java
+++ OOP/4_abstract_interface/shape_main.java
@@ -2,8 +2,16 @@ package OOP.4_abstract_interface;
 
 public class shape_main {
     public static void main(String[] args) {
-        Shape Cir_shape = new Circle(10);
-        // Shape Sq_shape = new Sqare(5);
+        Circle Cir_shape = new Circle(10);
+        Sqare Sq_shape = new Sqare(5);
+
+        System.out.println("Circle");
+        System.out.println("Area = " + Cir_shape.calArea());
+        System.out.println(Cir_shape.getColor());
+        System.out.println("\nSquare");
+        System.out.println("Area = " + Sq_shape.calArea());
+        System.out.println(Sq_shape.getColor());
+
     }
 
 }
